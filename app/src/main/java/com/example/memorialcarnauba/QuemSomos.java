332b617bc9756e37a03a16244ca50c9040da96e9
Ednardo Fernandes <ednardofernandes@alu.ufc.br>
2019-05-18 18:21:15 -0300

    Adicionado ViewPager na Activity Quem somos

--- app/src/main/java/com/example/memorialcarnauba/QuemSomos.java
+++ app/src/main/java/com/example/memorialcarnauba/QuemSomos.java
@@ -1,17 +1,30 @@
 package com.example.memorialcarnauba;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class QuemSomos extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private SliderAdapter sliderAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quem_somos);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+
+        sliderAdapter = new SliderAdapter(this);
+
+        mSlideViewPager.setAdapter(sliderAdapter);
     }
 }
